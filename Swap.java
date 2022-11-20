2f8b2d0e038cb07853f0c931b87c7b8368daa8e6
Hemantika <hemarpatel@yahoo.co.uk>
2022-11-20 18:47:40 +0000

    hello

---
+++ Swap.java
@@ -0,0 +1,11 @@
+package Swap;
+
+public class Swap {
+    public static void main(string args[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
